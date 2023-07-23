d19be413c609d565766fca27d3d6f5eddf94cef2
Roberto Leyva <roaleyva@gmail.com>
2023-07-22 22:36:33 -0700

    Add random number generator for each string

--- mixedMessages.js
+++ mixedMessages.js
@@ -5,3 +5,15 @@ const allPhrases = {
     string2: ['my lover', 'a dog', 'the queen', 'a zombie', 'an alien', 'my mom', 'my teacher', 'an egg', 'a sheep', 'a television', 'my best fiend', 'a ghost', 'my cat', 'Santa Claus', 'my school bag', 'my crush', 'a bear', 'a toothbrush', 'a unicorn', 'a cup of tea', 'my phone', 'my brother', 'a gun', ' my nose', 'a breakfast', 'the king', 'a milk', 'a book', 'a sword', '10,000 dollars', 'a chicken'],
     string3: ["because I'm sexy", "for good luck", "for god's sake", "while watching TV", "at my friend's house", "because I can", "while eating lunch", "because I hate that", "and I'm proud of it", "because it's my hobby", "covered with chocolate", "because I'm horny", "inside a car", "and I liked it", "covered with honey", "when my friend died", "and it was amazing", "to kill time", "because my brain is small", "because I'm awsome"]
 };
+
+const selectString1 = () => {
+    return Math.floor(Math.random() * allPhrases.string1.length);
+}
+
+const selectString2 = () => {
+    return Math.floor(Math.random() * allPhrases.string2.length);
+}
+
+const selectString3 = () => {
+    return Math.floor(Math.random() * allPhrases.string3.length);
+}
